835f199a4f336e3547fcb26514bed2ea42a0a4c4
Alistair000x <darmensun43@gmail.com>
2023-03-16 15:04:00 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main - entry point
- * Return: always 1 (success)
+ * Return: 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora karpor, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora karpor, 2015-10-19"));
 	return (1);
 }
