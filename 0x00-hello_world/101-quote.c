dc45a5f3e34168c981de871fb4d154a1fdeb8f03
Alistair000x <darmensun43@gmail.com>
2023-03-16 15:22:48 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora karpor, 2015-10-19\n",
+	write(2, "and that piece of art is useful\" - Dora korpor, 2015-10-19\n",
 59);
 	return (1);
 }
