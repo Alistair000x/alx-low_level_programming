24a58602ef1257d502d30e94c1066b4700b8bc8a
Alistair000x <darmensun43@gmail.com>
2023-04-27 19:59:57 -0700

    this is how u can Write a function that returns the number of elements in a linked list_t list.

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,20 +10,14 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t node_count = 1;
-
-	/*if head is null return 0 nodes*/
+	size_t pizza = 1;
 	if (h == NULL)
 		return (0);
-
-	/*iterate through the nodes*/
 	while (h->next != NULL)
 	{
-		/*go to the next node*/
 		h = h->next;
-		/*count node*/
-		node_count++;
+		pizza++;
 	}
 
-	return (node_count);
+	return (pizza);
 }
