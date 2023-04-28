2f8074a38eb4a6964d1e3c66b448ffc45fb18a8f
Alistair000x <darmensun43@gmail.com>
2023-04-27 20:10:32 -0700

    this is how u can Write a function that adds a new node at the end of a list_t list.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,42 +11,28 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *temp;
+	list_t *fqas, *atay;
 	size_t str_len = 0;
-
-	/*set strig length to 0 if its NULL*/
 	if (str == NULL)
 		str_len = 0;
-
-	/*count length of string*/
 	while (str[str_len] != '\0')
 		str_len++;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	fqas = malloc(sizeof(list_t));
+	if (fqas == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
-	new_node->len = str_len;
-	new_node->next = NULL;
-
-	/*copy our starting node pointer to temp*/
-	temp = *head;
-
-	/*if temp is NULL place our new_node at the beginning of the list*/
-	/*else if its not null we place our new_node to temp->next hence*/
-	/*adding our new_node at the end of the list*/
-	if (temp == NULL)
-		*head = new_node;
+	fqas->str = strdup(str);
+	fqas->len = str_len;
+	fqas->next = NULL;
+	atay = *head;
+	if (atay == NULL)
+		*head = fqas;
 	else
 	{
-		/**
-		 * we iterate pointer next addr if its not NULL our temp addr
-		 * points to the next addr or the next node.
-		*/
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new_node;
+		while (atay->next != NULL)
+			atay = atay->next;
+		atay->next = fqas;
 	}
 
 	return (*head);
