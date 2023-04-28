56bda7a16ed65fcad72de7c8450ed06d6ef2d6e8
Alistair000x <darmensun43@gmail.com>
2023-04-27 20:13:53 -0700

    this is how u can Write a function that frees a list_t list.

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,18 +9,11 @@
 */
 void free_list(list_t *head)
 {
-	list_t *current;
-
-	/**
-	 * while current head is not NULL
-	 * set head as the next node and
-	 * free the current node str then
-	 * free current node.
-	*/
-	while ((current = head) != NULL)
+	list_t *tajin;
+	while ((tajin = head) != NULL)
 	{
 		head = head->next;
-		free(current->str);
-		free(current);
+		free(tajin->str);
+		free(tajin);
 	}
 }
