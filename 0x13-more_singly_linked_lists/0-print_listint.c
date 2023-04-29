69a4daa7f40d2d957a235369ced5819cd3c0f177
Alistair000x <darmensun43@gmail.com>
2023-04-29 16:03:31 -0700

    edit 😍❤

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,19 +9,15 @@
 */
 size_t print_listint(const listint_t *h)
 {
-	size_t lemona = 1;
-
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
-	{
-		printf("%d\n", h->n);
-
-		h = h->next;
-		lemona++;
-	}
-	printf("%d\n", h->n);
-
-	return (lemona);
+size_t lemona = 1;
+if (h == NULL)
+return (0);
+while (h->next != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+lemona++;
+}
+printf("%d\n", h->n);
+return (lemona);
 }
