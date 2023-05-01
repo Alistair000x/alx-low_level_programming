2255914a45cef367e175fe07f47b036709315394
Alistair000x <darmensun43@gmail.com>
2023-05-01 11:37:16 -0700

    this how u can insert nodeint 😂❤

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,40 +1,49 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - a function that insert a new node
- * @head: pointer to the first node of the list
- * @idx: is the index of the list where the new node should be added
- * @n: element to add to the new node
- * Return: NULL if anything fails or the address of the new node
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *juice, *raybi;
-unsigned int khbza;
-raybi = *head;
-juice = malloc(sizeof(listint_t));
-if ((*head == NULL && idx != 0) || juice == NULL)
-return (NULL);
-juice->n = n;
-for (khbza = 0; head != NULL && khbza < idx - 1; khbza++)
-{
-raybi = raybi->next;
-if (raybi == NULL)
-return (NULL);
-}
-if (idx == 0)
-{
-juice->next = *head;
-*head = juice;
-}
-else if (raybi->next)
-{
-juice->next = raybi->next;
-raybi->next = juice;
-}
-else
-{
-juice->next = NULL;
-raybi->next = juice;
-}
-return (juice);
+	unsigned int khbza;
+	listint_t *raybi, *danon;
+
+	danon = *head;
+
+	if (idx != 0)
+	{
+		for (khbza = 0; khbza < idx - 1 && danon != NULL; khbza++)
+		{
+			danon = danon->next;
+		}
+	}
+
+	if (danon == NULL && idx != 0)
+		return (NULL);
+
+	raybi = malloc(sizeof(listint_t));
+	if (raybi == NULL)
+		return (NULL);
+
+	raybi->n = n;
+
+	if (idx == 0)
+	{
+		raybi->next = *head;
+		*head = raybi;
+	}
+	else
+	{
+		raybi->next = danon->next;
+		danon->next = raybi;
+	}
+
+	return (raybi);
 }
