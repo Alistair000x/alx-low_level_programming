eccec802b053e37b7ae37520e98d5e61ad4e6285
Alistair000x <darmensun43@gmail.com>
2023-07-10 13:26:18 -0700

    idk

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,66 +1,77 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 /**
- * a_file- checks if files can be opened
- * @filename: files
- * @files: files
+ * a_file - checks if files can be opened
+ * @filename: file descriptor
+ * @files: file descriptor
  * @argv: arguments
  * Return: no return
  */
 void a_file(int filename, int files, char *argv[])
 {
-if (filename == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s/n", argv[1]);
-exit(98);
+	if (filename == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (files == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
-if (files == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s/n", argv[2]);
-exit(99);
-}
-}
+
 /**
  * main - check the code
- * @argc: numbers of arg
- * @argv: arg
+ * @argc: number of args
+ * @argv: arguments
  * Return: 0.
  */
 int main(int argc, char *argv[])
 {
-int filename, files, mes;
-ssize_t k;
-ssize_t j;
-char boo[1024];
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "%s/n", "Usage: cp file_from filr_to");
-exit(97);
+	int filename, files, mes;
+	ssize_t k, j;
+	char boo[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	filename = open(argv[1], O_RDONLY);
+	files = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	a_file(filename, files, argv);
+
+	k = 1024;
+	while (k == 1024)
+	{
+		k = read(filename, boo, 1024);
+		if (k == -1)
+			a_file(-1, 0, argv);
+		j = write(files, boo, k);
+		if (j == -1)
+			a_file(0, -1, argv);
+	}
+
+	mes = close(filename);
+	if (mes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filename);
+		exit(100);
+	}
+	mes = close(files);
+	if (mes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", files);
+		exit(100);
+	}
+
+	return (0);
 }
-filename = open(argv[1], O_RDONLY);
-files = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-a_file(filename, files, argv);
-k = 1024;
-while (k == 1024)
-{
-k = read(filename, boo, 1024);
-if (k == -1)
-a_file(-1, 0, argv);
-j = write(files, boo, k);
-if (j == -1)
-a_file(0, -1, argv);
-}
-mes = close(filename);
-if (mes == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d/n", filename);
-exit(100);
-}
-mes = close(files);
-if (mes == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d/n", filename);
-exit(100);
-}
-return (0);
-}
+
